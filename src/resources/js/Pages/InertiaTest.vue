d08a8aec8e57482a30fd9689c680b8d64cef9724
tbass44 <ts0815@live.jp>
2025-02-20 21:38:27 +0900

    設定一旦完了

---
+++ src/resources/js/Pages/InertiaTest.vue
@@ -0,0 +1,9 @@
+<script setup>
+import { Link } from '@inertiajs/vue3';
+</script>
+
+<template>
+  Inertiaテストです。<br>
+  <a href="/">aタグでWelcomeに移動</a><br>
+  <Link href="/">LinkでWelcomeに移動</Link>
+</template>
--- src/routes/web.php
+++ src/routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\InertiaTestController;
 
 /*
 |--------------------------------------------------------------------------
@@ -34,5 +35,9 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('/inertia-test', function () {
+    return Inertia::render('InertiaTest');
+    });
 
 require __DIR__.'/auth.php';
